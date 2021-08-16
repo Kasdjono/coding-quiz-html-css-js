baa8dd6bce43b431d7eb2c5868b73b10ab85d69d
Nick_Kash <nicholas.kasdjono.777@my.csun.edu>
2021-08-15 22:13:46 -0700

    started html, css, and js file

---
+++ script.js
@@ -0,0 +1,25 @@
+var startEl = document.querySelector("start");
+var optionA_El = document.querySelector('#A');
+var optionB_El = document.querySelector('#B');
+var optionC_El = document.querySelector('#C');
+var optionD_El = document.querySelector('#D');
+
+
+startEl.addEventListener("click", function() {
+    codeQuiz();
+});
+
+
+// goes through questions 1-7
+
+// 4 buttons to select on the first 4 questions
+
+// input typted  code for the last 3 questions
+
+// is timed and there is a timer on the screen
+
+// 10 seconds will be removed per wrong answer
+
+// tracks score of user
+
+// at the end it will give the score and save the username 
